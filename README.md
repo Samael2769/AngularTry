90dd04d76b67700b1005ebf34f6bf371746e3f9c
Samael <jeremie.joannard@epitech.eu>
2023-06-02 06:10:43 +0200

    [MOD] ../README.md, [MOD] src/app/app.component.ts, [MOD] src/app/home/home.component.ts, [MOD] src/app/housing-location/housing-location.component.ts, [MOD] src/main.ts, [ADD] db.json, [ADD] src/app/details/, [ADD] src/app/housing.service.ts, [ADD] src/app/routes.ts,
    Housing app final

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # AngularTry
 Just trying Angular 
+
+## Angular
+### Install
+```
+npm install -g @angular/cli
+```
+### Run
+```
+ng serve
+```
+
+## DB
+### Install
+```
+npm install -g json-server
+```
+### Run
+```
+json-server --watch db.json
+```
--- angular-try/src/app/home/home.component.ts
+++ angular-try/src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { Housinglocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -13,25 +14,35 @@ import { Housinglocation } from '../housinglocation';
   template: `
   <section>
     <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
+      <input type="text" placeholder="Filter by city" #filter>
+      <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
     </form>
   </section>
   <section class="results">
-    <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+    <app-housing-location 
+    *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation">
+    </app-housing-location>
   </section>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  housingLocation: Housinglocation = {
-    id: 0,
-    name: 'Sam Home',
-    city: 'Sam city',
-    state: 'HE',
-    photo: 'https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/07/download-23.jpg',
-    availableUnits: 99,
-    wifi: true,
-    laundry: true,
-  };
+  housingLocationList: Housinglocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: Housinglocation[] = [];
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+  
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: Housinglocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
 }
